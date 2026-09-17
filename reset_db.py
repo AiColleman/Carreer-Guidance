05a0c80a30b2181246ab3a49c162d752303f7ef4
Camilo <camiloharol80@gmail.com>
2026-09-17 00:43:14 -0500

    feat: add database reset script

---
+++ reset_db.py
@@ -0,0 +1,5 @@
+import urllib.request
+import json
+
+# We don't have psycopg2 installed by default, so let's use the REST API if possible?
+# No, we can just install psycopg2-binary or pg8000 using pip.
