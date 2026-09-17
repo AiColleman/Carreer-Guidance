e666467241a41de567382159bac1c4ee96db1d19
Camilo <camiloharol80@gmail.com>
2026-09-17 01:37:33 -0500

    chore: enable developer exception page in production to debug

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -47,10 +47,7 @@ using (var scope = app.Services.CreateScope())
 }
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-}
+app.UseDeveloperExceptionPage();
 
 // Render provides the PORT environment variable. We should listen on it.
 var port = Environment.GetEnvironmentVariable("PORT") ?? "80";
